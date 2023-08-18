f844fd2d5d4e34ff600f3362cbc1c75458ed411a
zimbaan <kapiladhithya@gmail.com>
2023-08-18 21:12:34 +0530

    calculating cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,7 +79,18 @@ document.querySelectorAll('.js-add-to-cart ').forEach((button) => {
         quantity: 1
       });
     }
+
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+
+
     
-    console.log(cart);
   })
 })
+
