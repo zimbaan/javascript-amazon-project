fe1a3598317e62249bf80ddb41041799e808b0f0
zimbaan <kapiladhithya@gmail.com>
2023-08-18 22:22:49 +0530

    move cart function to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,27 @@
 export const cart=[];
+
+function AddToCart(productId){
+  let selectquantity; 
+
+    
+  document.querySelectorAll(`.js-quantity-selector-${productId}`).forEach((select) => {
+    selectquantity = parseInt(select.value);
+  })
+
+  let matchingItem;
+  cart.forEach((item) => {
+    if(productId === item.productId){
+    matchingItem = item;
+    }
+  
+  });
+
+  if(matchingItem){
+    matchingItem.quantity+=selectquantity;
+  }else{
+    cart.push({
+      productId: productId,
+      quantity: selectquantity,
+    });
+  }
+}
